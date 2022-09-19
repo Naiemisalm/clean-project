2ab1ad3d33bfcf8177252fc21f02fd0ebbc90f2c
Naiemisalm <nayemislam30964@gmail.com>
2022-09-19 20:00:44 +0600

    public route

--- src/App.js
+++ src/App.js
@@ -7,16 +7,29 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Servies from './pages/Servies';
 
+
+const PublicRoute = [
+  {path:"/", name: "Home", Component: Home},
+  {path:"/about", name: "About", Component: About},
+  {path:"/servies", name: "Servies", Component: Servies},
+  {path:"/contact", name: "Contact", Component: Contact},
+  {path:"/login", name: "Login", Component: Login},
+
+];
+
 function App() {
   return (
     <div>
       <Navbar>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        {/* <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/servies' element={<Servies/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/login' element={<Login/>}></Route> */}
+
+        {PublicRoute.map((route) => ( <Route path={route.path} element={<route.Component/>} /> ))}
+
       </Routes>
       </Navbar>
     </div>
